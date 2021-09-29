8273fa0bbca9ac1cd9c533b4bf02f771c0ca478c
aliraad79 <aliraad79@gmail.com>
2021-09-29 15:01:44 +0330

    it's working!

--- compiler.py
+++ compiler.py
@@ -24,33 +24,37 @@ class LexicalError(Enum):
 
 
 class Node:
-    def __init__(self, char, nexts: List["Node"], is_start=False, is_end=False):
+    def __init__(self, char, nexts: List["Node"] = [], is_start=False, is_end=False):
         self.char = char
         self.nexts = nexts
         self.is_start = is_start
         self.is_end = is_end
-    
+
     def __repr__(self):
-        return self.char
+        return f"{self.char} with {len(self.nexts) if self.nexts else 0} nexts"
 
 
-DFA_TREE: List[Node] = []
+class Token:
+    def __init__(self, _type: TokenType, lexeme):
+        self.type = _type
+        self.lexeme = lexeme
 
 
 def create_dfa_tree():
     symbols = [";", ":", ",", "[", "]", "(", ")", "{", "}", "+", "-", "*", "=", "<"]
     symbol_tree = []
     for i in symbols:
-        symbol_tree.append(Node(i, None, is_start=True, is_end=True))
-    symbol_tree.append(Node("=", [Node("=", None, is_end=True)], is_start=True))
+        symbol_tree.append(Node(i, [], is_start=True, is_end=True))
+    symbol_tree.append(Node("=", [Node("=", [], is_end=True)], is_start=True))
+
     whitespace_tree = []
-    white = ["\x09", "\x0A", "\x0B", "\x0C", "\x20"]
-    for i in white:
-        whitespace_tree.append(Node(i, None, is_start=True, is_end=True))
+    white_spaces = ["\x09", "\x0A", "\x0B", "\x0C", "\x20"]
+    for i in white_spaces:
+        whitespace_tree.append(Node(i, [], is_start=True, is_end=True))
 
     for i in range(10):
         exec(
-            f'node_{i} = Node("{i}", None, is_start=True, is_end=True)',
+            f'node_{i} = Node("{i}", [], is_start=True, is_end=True)',
             locals(),
             globals(),
         )
@@ -69,32 +73,32 @@ def create_dfa_tree():
     # fmt: on
     keyword_tree = []
     # if
-    No_f = Node("f", None, is_end=True)
+    No_f = Node("f", [], is_end=True)
     No_i = Node("i", [No_f], is_start=True)
     keyword_tree.append(No_i)
 
     # int
-    No_t = Node("t", None, is_end=True)
+    No_t = Node("t", [], is_end=True)
     No_n = Node("n", [No_t])
     No_i = Node("i", [No_n], is_start=True)
     keyword_tree.append(No_i)
 
     # else
-    No_e = Node("e", None, is_end=True)
+    No_e = Node("e", [], is_end=True)
     No_s = Node("s", [No_e])
     No_l = Node("l", [No_s])
     No_e = Node("e", [No_l], is_start=True)
     keyword_tree.append(No_e)
 
     # void
-    No_d = Node("d", None, is_end=True)
+    No_d = Node("d", [], is_end=True)
     No_i = Node("i", [No_d])
     No_o = Node("o", [No_i])
     No_v = Node("v", [No_o], is_start=True)
     keyword_tree.append(No_v)
 
     # repeat
-    No_t = Node("t", None, is_end=True)
+    No_t = Node("t", [], is_end=True)
     No_a = Node("a", [No_t])
     No_e = Node("e", [No_a])
     No_p = Node("p", [No_e])
@@ -103,7 +107,7 @@ def create_dfa_tree():
     keyword_tree.append(No_r)
 
     # break
-    No_k = Node("k", None, is_end=True)
+    No_k = Node("k", [], is_end=True)
     No_a = Node("a", [No_k])
     No_e = Node("e", [No_a])
     No_r = Node("r", [No_e])
@@ -111,7 +115,7 @@ def create_dfa_tree():
     keyword_tree.append(No_b)
 
     # untill
-    No_l = Node("l", None, is_end=True)
+    No_l = Node("l", [], is_end=True)
     No_l = Node("l", [No_l])
     No_i = Node("i", [No_l])
     No_t = Node("t", [No_i])
@@ -120,7 +124,7 @@ def create_dfa_tree():
     keyword_tree.append(No_u)
 
     # return
-    No_n = Node("n", None, is_end=True)
+    No_n = Node("n", [], is_end=True)
     No_r = Node("r", [No_n])
     No_u = Node("u", [No_r])
     No_t = Node("t", [No_u])
@@ -131,11 +135,11 @@ def create_dfa_tree():
     id_tree = []
 
     for i in range(11):
-        exec(f'node_{i} = Node("{i}", None, is_end=True)', locals(), globals())
+        exec(f'node_{i} = Node("{i}", [], is_end=True)', locals(), globals())
         exec(f"id_tree.append(node_{i})", locals(), globals())
     for alphabet in string.ascii_letters:
         exec(
-            f'node_{alphabet} = Node("{alphabet}", None, is_start=True, is_end=True)',
+            f'node_{alphabet} = Node("{alphabet}", [], is_start=True, is_end=True)',
             locals(),
             globals(),
         )
@@ -147,16 +151,17 @@ def create_dfa_tree():
     return [*symbol_tree, *whitespace_tree, *number_tree, *keyword_tree, *id_tree]
 
 
-class Token:
-    def __init__(self, _type: TokenType, lexeme):
-        self.type = _type
-        self.lexeme = lexeme
+pointer: int = 0
+buffer: List[str] = []
 
 
-def get_next_token():
+def get_next_char():
+    global pointer, buffer
+
+    pointer += 1
     with open("input.txt", "r") as file:
         char = file.read(pointer)
-
+    buffer.append(char[pointer - 1 :])
     return char[pointer - 1 :]
 
 
@@ -183,19 +188,55 @@ def print_pretty(char):
         phrase = "\\t"
     elif char == " ":
         phrase = "space"
-    print(f" {phrase} ", end="")
+    elif char == "":
+        phrase = "EOF"
+    return phrase
+
+
+def get_buffffer(to_end=True):
+    global buffer
+    l = len(buffer) if to_end else len(buffer) - 1
+    i = buffer[:l]
+    if to_end:
+        buffer = []
+    else:
+        # print("BUGFER", buffer)
+        buffer = [buffer[l]]
+    return i
 
 
 lineno: int = 0
-pointer: int = 0
 if __name__ == "__main__":
     dfa_tree = create_dfa_tree()
     while True:
-        pointer += 1
-        try:
-            token = get_next_token()
-            print_pretty(token)
-            if token == "":
-                break
-        except IndexError:
+        if buffer == []:
+            charachter = get_next_char()
+            next_charachter = get_next_char()
+        else:
+            charachter = buffer[-1]
+            next_charachter = get_next_char() if len(buffer) <= 1 else buffer[-2]
+
+        selected_nodes = [node for node in dfa_tree if node.char == charachter]
+
+        # print_pretty(charachter)
+
+        is_valid = False
+
+        # Algo
+        if len(selected_nodes) == 0:
+            print("TOKEN is ", "".join(get_buffffer(False)))
+        for option in selected_nodes:
+            for node in option.nexts:
+                if node.char == next_charachter:
+                    is_valid = True
+                    break
+        if is_valid:
+            charachter = next_charachter
+            next_charachter = get_next_char()
+        else:
+            print("TOKEN is ", print_pretty("".join(get_buffffer(False))))
+
+        if charachter == "\n":
+            lineno += 1
+        if charachter == "":
             break
