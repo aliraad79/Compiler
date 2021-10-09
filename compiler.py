476b7000e04ee6986aa8ca05c3cbbb3e548e85de
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-10-09 16:31:09 +0330

    Improve algorithm

--- compiler.py
+++ compiler.py
@@ -1,10 +1,16 @@
 # Jafar Sadeghi 97
 # Ali Ahmadi Kafeshani 97105703
 # No prints please
+
+from __future__ import annotations
 from enum import Enum
-from typing import List
-from utils import print_pretty, print_log
-from file import add_tokens_to_file, add_symbols_to_symbol_table
+from typing import List, Tuple
+from utils import (
+    print_log,
+    add_tokens_to_file,
+    add_symbols_to_file,
+    add_errors_to_file,
+)
 import re
 
 
@@ -25,30 +31,6 @@ class LexicalError(Enum):
     INVALID_NUMBER = "Invalid number"
 
 
-class Node:
-    def __init__(
-        self,
-        char,
-        nexts: List["Node"] = [],
-        is_start=False,
-        is_end=False,
-        is_universal=False,
-        next_universal_nodes: List["Node"] = [],
-    ):
-        self.char = char
-        self.nexts = nexts
-        self.is_start = is_start
-        self.is_end = is_end
-        self.is_universal = is_universal
-        self.next_universal_nodes = next_universal_nodes
-
-    def is_equal(self, other):
-        return True if self.is_universal else self.char == other
-
-    def __repr__(self):
-        return f"{print_pretty(self.char)} with {len(self.nexts) if self.nexts else 0} nexts"
-
-
 class Token:
     def __init__(self, _type: TokenType, lexeme):
         self.type = _type
@@ -66,10 +48,15 @@ def get_next_char():
 
 
 def add_token_to_array(token) -> None:
+    if token == "":
+        return None
     _type = ""
     for regex in TokenType:
         if re.search(regex.value, token):
             _type = regex
+
+    if _type == "":
+        error_dict.setdefault(line_number, []).append((LexicalError.INVALID_INPUT.value,))
 
     if _type not in ["", TokenType.WHITESPACE, TokenType.COMMENT]:
         token_dict.setdefault(line_number, []).append(Token(_type.name, token))
@@ -89,17 +76,17 @@ def get_token_from_buffer():
 # global vars
 token_dict: dict[int : List[Token]] = {}
 symbol_list: set[str] = set()
-error_list: List[str] = []
+error_dict: dict[int:Tuple] = {}
 
 buffer: List[str] = []
 line_number: int = 0
 char_pointer = 0
 
 if __name__ == "__main__":
-    from dfa_tree import create_dfa_tree
+    from dfa_tree import create_dfa_tree, State
 
-    dfa_tree = create_dfa_tree()
-    next_selected_nodes = []
+    dfa_mother_state_tree = create_dfa_tree()
+    next_selected_state = None
     while True:
 
         # Fill char and next char with buffer otherwise get them from input file
@@ -119,59 +106,49 @@ if __name__ == "__main__":
             break
 
         # dfa nodes that is in start position an can be continued
-        if next_selected_nodes == []:
-            selected_nodes = [node for node in dfa_tree if node.char == char]
+        if next_selected_state == None:
+            selected_state: State = dfa_mother_state_tree.next_dfa_tree_state(char)
         else:
-            selected_nodes = next_selected_nodes
-            next_selected_nodes = []
+            selected_state = next_selected_state
+            next_selected_state = None
+
         # Algorithm
 
         # No way to go in dfa tree --> possible token in buffer
-        if len(selected_nodes) == 0:
+        if selected_state == None:
             add_token_to_array(get_token_from_buffer())
 
         # Check if next character possibly can be append to our current state
-        can_be_continued = False
-        is_reach_end_of_universal = False
-        next_nodes = []
-        not_universal_next_nodes = []
-        for option in selected_nodes:
-            for node in option.nexts:
-                if node.is_equal(next_char):
-                    next_nodes.append(node)
-                    can_be_continued = True
-                    for j in option.next_universal_nodes:
-                        if j.char == next_char:
-                            not_universal_next_nodes.append(j)
-                            is_reach_end_of_universal = True
+        else:
+            next_state = selected_state.next_dfa_tree_state(next_char)
+
+        can_be_continued = next_state != None
 
         if buffer == [] and not can_be_continued:
-            error_list.append(LexicalError.INVALID_INPUT.value + str(line_number))
-        if True:
+            error_dict.setdefault(line_number, []).append((LexicalError.INVALID_INPUT.value,))
+
+        if line_number == 6 and False:
             print_log(
                 buffer,
                 char,
                 next_char,
-                selected_nodes,
+                selected_state,
                 can_be_continued,
-                is_reach_end_of_universal,
-                not_universal_next_nodes,
-                line_number=21,
+                line_number=7,
             )
-        if can_be_continued and not is_reach_end_of_universal:
+
+        if can_be_continued:
             char = next_char
             next_char = get_next_char()
-            next_selected_nodes = (
-                next_nodes
-                if not_universal_next_nodes == []
-                else not_universal_next_nodes
-            )
+            next_selected_state = next_state
         else:
             add_token_to_array(get_token_from_buffer())
+            next_selected_state = None
 
         # next line
         if char == "\n":
             line_number += 1
 
     add_tokens_to_file(line_number, token_dict)
-    add_symbols_to_symbol_table(symbol_list)
+    add_symbols_to_file(symbol_list)
+    add_errors_to_file(line_number, error_dict)
