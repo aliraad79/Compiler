4ffd584f09f5d57343341951c65859b2472a0240
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-11-11 10:41:16 +0330

    commit somethings

--- parse_tree.py
+++ parse_tree.py
@@ -1,26 +1,65 @@
-graph = {
-    "A": ["B", "C"],
-    "B": ["D"],
-    "C": ["E", "F"],
-    "D": ["G", "H"],
-    "F": ["I", "J", "K"],
-    "E": [],
-    "G": [],
-    "J": [],
-    "K": [],
-    "H": [],
-    "I": [],
-}
+# graph = {
+#     "A": ["B", "C"],
+#     "B": ["D"],
+#     "C": ["E", "F"],
+#     "D": ["G", "H"],
+#     "F": ["I", "J", "K"],
+#     "E": [],
+#     "G": [],
+#     "J": [],
+#     "K": [],
+#     "H": [],
+#     "I": [],
+# }
 
-def print_tree(graph, node, depth):
-    if depth == 0:
-        print(node)
-    elif depth == 1:
-        print("|---", node)
-    else:
-        print("|   ", "\t" * (depth - 2), "|---", node)
-    for i in graph[node]:
-        print_tree(graph, i, depth + 1)
+# def print_tree(graph, node, depth):
+#     if depth == 0:
+#         print(node)
+#     elif depth == 1:
+#         print("|---", node)
+#     else:
+#         print("|   ", "\t" * (depth - 2), "|---", node)
+#     for i in graph[node]:
+#         print_tree(graph, i, depth + 1)
 
 
-print_tree(graph, "A", 0)
+# print_tree(graph, "A", 0)
+from scanner import Token
+from typing import List
+
+
+class ParseTreeEdge:
+    def __init__(
+        self,
+        next_node: "ParseTreeNode" = None,
+        token: Token = None,
+        
+    ):
+        self.next_node = next_node
+        self.token = token
+
+    def __repr__(self):
+        return f"Edge<next_node = {self.next_node}, token = {self.token}>"
+
+
+class ParseTreeNode:
+    def __init__(self, next_nodes: List[ParseTreeEdge] = []):
+        self.next_nodes = next_nodes
+
+    def next_parse_state(self, other: Token):
+        for i in self.next_nodes:
+            if i.token == other:
+                return i
+
+    def __repr__(self):
+        return f"ParseTreeNode<next_nodes = {self.next_nodes}>"
+
+
+def get_parse_tree():
+    program = ParseTreeNode()
+    
+    declaration_list_node = None
+    declaration_list = ParseTreeEdge(declaration_list_node,)
+
+    program.next_nodes.append(declaration_list)
+    return
--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 from scanner import Scanner
 from utils import write_parse_tree_to_file, write_syntax_errors_to_file
-
+from parse_tree import get_parse_tree
 
 class Parser:
     def __init__(self, scanner: Scanner):
         self.scanner = scanner
         self.curret_token = ""
-        self.parse_tree = ...
+        self.parse_tree = get_parse_tree()
+        self.stack = []
         self.syntax_errors = []
 
     def get_next_token(self):
@@ -15,6 +16,15 @@ class Parser:
     def save_to_file(self):
         write_syntax_errors_to_file(self.syntax_errors)
         write_parse_tree_to_file(self.parse_tree)
+
+    def add_token_to_stack(self, token):
+        self.stack.insert(0, token)
+
+    def pop_stack_head(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        # raise error maybe
+        return None
 
     def start(self):
         print("INIT")
