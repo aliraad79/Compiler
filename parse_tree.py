29884ae9926d266debefefdd49cc41aad0bc13c2
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-11-18 16:30:42 +0330

    Improvefirst and folows

--- parse_tree.py
+++ parse_tree.py
@@ -1,17 +1,3 @@
-# graph = {
-#     "A": ["B", "C"],
-#     "B": ["D"],
-#     "C": ["E", "F"],
-#     "D": ["G", "H"],
-#     "F": ["I", "J", "K"],
-#     "E": [],
-#     "G": [],
-#     "J": [],
-#     "K": [],
-#     "H": [],
-#     "I": [],
-# }
-
 # def print_tree(graph, node, depth):
 #     if depth == 0:
 #         print(node)
@@ -23,43 +9,38 @@
 #         print_tree(graph, i, depth + 1)
 
 
-# print_tree(graph, "A", 0)
 from scanner import Token
 from typing import List
 
 
-class ParseTreeEdge:
-    def __init__(
-        self,
-        next_node: "ParseTreeNode" = None,
-        token: Token = None,
-        
-    ):
-        self.next_node = next_node
-        self.token = token
-
-    def __repr__(self):
-        return f"Edge<next_node = {self.next_node}, token = {self.token}>"
-
-
 class ParseTreeNode:
-    def __init__(self, next_nodes: List[ParseTreeEdge] = []):
+    def __init__(self, next_nodes: List["ParseTreeNode"] = []):
         self.next_nodes = next_nodes
-
-    def next_parse_state(self, other: Token):
-        for i in self.next_nodes:
-            if i.token == other:
-                return i
 
     def __repr__(self):
         return f"ParseTreeNode<next_nodes = {self.next_nodes}>"
 
 
+class Terminal(ParseTreeNode):
+    def next_parse_state(self, other: Token):
+        for i in self.next_edges:
+            if i.token == other:
+                return i
+
+
+class NonTerminal(ParseTreeNode):
+    ...
+
+
 def get_parse_tree():
+    o = []
+    with open('firsts.txt', 'r') as file:
+        a = list(map(str.split,file.readlines()))
+    for i in a:
+        o.append(NonTerminal())
     program = ParseTreeNode()
-    
-    declaration_list_node = None
-    declaration_list = ParseTreeEdge(declaration_list_node,)
+
+
 
     program.next_nodes.append(declaration_list)
     return
--- parser.py
+++ parser.py
@@ -2,29 +2,23 @@ from scanner import Scanner
 from utils import write_parse_tree_to_file, write_syntax_errors_to_file
 from parse_tree import get_parse_tree
 
+
 class Parser:
     def __init__(self, scanner: Scanner):
         self.scanner = scanner
         self.curret_token = ""
         self.parse_tree = get_parse_tree()
-        self.stack = []
+        self.nodes_buffer = []
         self.syntax_errors = []
 
     def get_next_token(self):
-        self.curret_token = self.scanner.get_next_token()
+        token = self.scanner.get_next_token()
+        self.curret_token = token
 
     def save_to_file(self):
         write_syntax_errors_to_file(self.syntax_errors)
         write_parse_tree_to_file(self.parse_tree)
 
-    def add_token_to_stack(self, token):
-        self.stack.insert(0, token)
-
-    def pop_stack_head(self):
-        if len(self.stack) > 0:
-            return self.stack.pop()
-        # raise error maybe
-        return None
-
     def start(self):
+        self.get_next_token()
         print("INIT")
