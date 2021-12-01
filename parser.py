2c6379e20db3d40b07ba06788e061cdcf8456721
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-12-01 14:22:08 +0330

    add parse tree as a dict

--- parser.py
+++ parser.py
@@ -11,16 +11,17 @@ from parse_tree import init_transation_diagrams, ParseTreeNode
 class Parser:
     def __init__(self, scanner: Scanner):
         self.scanner = scanner
-        self.curret_token: Token = None
+        self.current_token: Token = None
         self.transation_diagrams: dict[str, ParseTreeNode] = init_transation_diagrams()
         self.current_node: ParseTreeNode = self.transation_diagrams["program"]
-        self.parse_tree_repr = []  # For test
+        self.parse_tree_repr: dict[int, str] = {}  # For test
         self.nodes_buffer: List[str] = []
         self.return_nodes: List[ParseTreeNode] = []
         self.syntax_errors: List = []
+        self.current_depth = 0
 
     def get_next_token(self):
-        self.curret_token = self.scanner.get_next_token()
+        self.current_token = self.scanner.get_next_token()
 
     def save_to_file(self):
         write_syntax_errors_to_file(self.syntax_errors)
@@ -28,22 +29,23 @@ class Parser:
 
     def log(self):
         print_parser_log(
-            self.current_node, self.curret_token, self.return_nodes, self.nodes_buffer
+            self.current_node, self.current_token, self.return_nodes, self.nodes_buffer
         )
+
+    def add_to_parse_tree(self, string):  # or maybe get token obj ?
+        self.parse_tree_repr.setdefault(self.current_depth, []).append(string)
 
     def start(self):
         try:
-            print("INIT")
             self.get_next_token()
             terminal = False
-            while self.curret_token.lexeme != "$":
-                self.log()
+            while self.current_token.lexeme != "$":
                 if self.current_node:
                     (
                         self.current_node,
                         terminal,
                     ) = self.current_node.next_parse_tree_node(
-                        self.curret_token,
+                        self.current_token,
                         self.nodes_buffer,
                     )
                     if len(self.nodes_buffer) != 0:
@@ -53,14 +55,16 @@ class Parser:
                             self.nodes_buffer.pop(len(self.nodes_buffer) - 1)
                         ]
                     if terminal:
-                        self.parse_tree_repr.append(self.curret_token.lexeme)
+                        self.add_to_parse_tree(self.current_token.lexeme)
                         self.get_next_token()
                         terminal = False
                 else:
+                    self.current_depth += 1
                     self.current_node = self.return_nodes.pop(
                         len(self.return_nodes) - 1
                     )
         except:
+            self.log()
             raise
         finally:
             print(self.parse_tree_repr)
