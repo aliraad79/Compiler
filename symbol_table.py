ee974a42212e471092cb9389cdda31306c831fed
aliraad79 <a.ahmadi.k.79@gmail.com>
2022-01-06 13:01:05 +0330

    move vars addres toi symbol table

--- symbol_table.py
+++ symbol_table.py
@@ -5,7 +5,8 @@ from utils import add_symbols_to_file
 class SymbolTable:
     def __init__(self) -> None:
         self.table: Dict[str, int] = {}
-        self.addres_pointer = 100
+        self.declared_symbols = []
+        self.addres_pointer = 500
 
     def insert(self, lexeme: str) -> None:
         self.table[lexeme] = self.addres_pointer
@@ -22,3 +23,11 @@ class SymbolTable:
 
     def save_to_file(self) -> None:
         add_symbols_to_file(self.get_symbols())
+
+    def get_temp(self) -> int:
+        t = self.addres_pointer
+        self.addres_pointer += 4
+        return t
+
+    def add_declared_symbols(self, lexeme) -> None:
+        self.declared_symbols.append(lexeme)
